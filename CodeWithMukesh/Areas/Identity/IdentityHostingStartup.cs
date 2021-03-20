9d2e52e33ebeb6549eabf0744cca528bb4edb370
Angel Colmenares <angel.ignacio.colmenares@gmail.com>
2021-03-20 16:07:21 -0500

    scaffolding identity,  Add A Default Role To Newly Registered User

---
+++ CodeWithMukesh/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using CodeWithMukesh.Auth;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CodeWithMukesh.Areas.Identity.IdentityHostingStartup))]
+namespace CodeWithMukesh.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
