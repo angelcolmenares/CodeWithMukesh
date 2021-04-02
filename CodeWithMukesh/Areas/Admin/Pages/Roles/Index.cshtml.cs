119546cbcee1b7aa3b097315e6a6d220453174bd
Angel Colmenares <angel.ignacio.colmenares@gmail.com>
2021-04-02 18:40:53 -0500

    view all

--- CodeWithMukesh/Areas/Admin/Pages/Roles/Index.cshtml.cs
+++ CodeWithMukesh/Areas/Admin/Pages/Roles/Index.cshtml.cs
@@ -16,6 +16,8 @@ namespace CodeWithMukesh.Areas.Admin.Pages.Roles
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
+        public List<RoleViewModel> RoleViewModelList {get;set;}
+
         public IndexModel(UserManager<ApplicationUser> userManager,
                           RoleManager<IdentityRole> roleManager)
         {
@@ -23,17 +25,22 @@ namespace CodeWithMukesh.Areas.Admin.Pages.Roles
             this._roleManager = roleManager;
         }
 
-        public async Task OnGet()
+        public async Task OnGetAsync()
         {
             System.Console.WriteLine("Roles  On Get............");
-            await Task.CompletedTask;
+            await LoadModel();
         }
 
         public async Task<IActionResult> OnGetViewAllAsync()
         {
-            var model = await _roleManager.Roles.Select(f=> new RoleViewModel{ Name= f.Name, Id= f.Id }).ToListAsync();            
-            return Partial("_ViewAll", model);
+            await LoadModel();
+            return Partial("_ViewAll", RoleViewModelList);
             
+        }
+
+        private async Task LoadModel()
+        {
+            RoleViewModelList = await _roleManager.Roles.Select(f=> new RoleViewModel{ Name= f.Name, Id= f.Id }).ToListAsync(); 
         }
 
     }
